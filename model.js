8ef37a62eaffe3af32c18845d6c2a5378a675382
Ian Pennington <irpennin@gmail.com>
2019-04-13 16:44:47 -0400

    revamp
    - smoothed, shrank, and reoriented model
    - added new skybox
    - hopefully support for HMD
    - some cleanup

--- model.js
+++ model.js
@@ -1,17 +1,133 @@
+var mesh, skyBox;
+
+if (WEBGL.isWebGLAvailable() === false) {
+
+    document.body.appendChild(WEBGL.getWebGLErrorMessage());
+
+}
+
+function getVRDisplay(onDisplay) {
+
+    if ('getVRDisplays' in navigator) {
+
+        navigator.getVRDisplays()
+            .then(function (displays) {
+                onDisplay(displays[0]);
+            });
+
+    }
+
+}
+
+var container;
+var camera, scene, renderer, light;
+var controls, water, sphere;
 
 
-var camera, cameraCube, controls, scene, renderer,
-    geometry, material, mesh, light1, stats;
 
-function trim(str) {
-    str = str.replace(/^\s+/, '');
-    for (var i = str.length - 1; i >= 0; i--) {
-        if (/\S/.test(str.charAt(i))) {
-            str = str.substring(0, i + 1);
-            break;
+function init() {
+
+    container = document.getElementById('container');
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000);
+
+    light = new THREE.DirectionalLight(0xffffff, 0.8);
+    scene.add(light);
+
+    // Skybox
+
+    var loader = new THREE.CubeTextureLoader();
+    loader.setPath('./uw_sky/underwater/');
+    var textureCube = loader.load(['uw_bk.jpg', 'uw_ft.jpg', 'uw_dn.jpg', 'uw_up.jpg', 'uw_rt.jpg', 'uw_lf.jpg']);
+
+    var skyMaterial = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        envMap: textureCube,
+        side: THREE.BackSide
+    });
+
+    var skyGeometry = new THREE.BoxGeometry(1024, 1024, 1024);
+
+    var skyMesh = new THREE.Mesh(skyGeometry, skyMaterial);
+
+    skyBox = new THREE.Object3D();
+    skyBox.add(skyMesh);
+
+    scene.add(skyBox);
+
+    // get fish model
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200 || xhr.status == 0) {
+                var rep = xhr.response; // || xhr.mozResponseArrayBuffer;
+                console.log(rep);
+                parseStlBinary(rep);
+                mesh.position.z = -2;
+                mesh.position.y = 0;
+                console.log('done parsing');
+            }
         }
     }
-    return str;
+    xhr.onerror = function (e) {
+        console.log(e);
+    }
+
+    xhr.open("GET", 'bellator.stl', true);
+    xhr.responseType = "arraybuffer";
+    xhr.send(null)
+
+
+    document.body.appendChild(WEBVR.createButton(renderer));
+    renderer.vr.enabled = true;
+
+    getVRDisplay(function (display) {
+        renderer.vr.setDevice(display);
+    });
+
+
+    //
+    /*
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls.maxPolarAngle = Math.PI * 0.495;
+    controls.target.set( 0, 10, 0 );
+    controls.minDistance = 40.0;
+    controls.maxDistance = 200.0;
+    controls.update();
+    */
+
+    //
+
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+    renderer.setAnimationLoop(render);
+}
+
+function render() {
+
+    if (mesh) {
+        mesh.rotation.y += 0.02;
+    }
+
+    skyBox.position.copy(camera.position);
+    renderer.render(scene, camera);
 }
 
 // Notes:
@@ -84,194 +200,7 @@ var parseStlBinary = function (stl) {
     scene.add(mesh);
 
     stl = null;
-
-    return mesh;
 };
-
-if (WEBGL.isWebGLAvailable() === false) {
-
-    document.body.appendChild(WEBGL.getWebGLErrorMessage());
-
-}
 
 init();
 animate();
-
-function init() {
-
-    //Detector.addGetWebGLMessage();
-
-    scene = new THREE.Scene();
-    sceneCube = new THREE.Scene();
-
-    textureCube = new THREE.TextureLoader().load("background.png");
-    textureCube.format = THREE.PNGFormat;
-    textureCube.mapping = THREE.CubeReflectionMapping;
-    textureCube.encoding = THREE.sRGBEncoding;
-
-    var cubeShader = THREE.ShaderLib["cube"];
-    var cubeMaterial = new THREE.ShaderMaterial({
-        fragmentShader: cubeShader.fragmentShader,
-        vertexShader: cubeShader.vertexShader,
-        uniforms: cubeShader.uniforms,
-        depthWrite: false,
-        side: THREE.BackSide
-    });
-
-    cubeMaterial.uniforms["tCube"].value = textureCube;
-    Object.defineProperty(cubeMaterial, 'map', {
-
-        get: function () {
-
-            return this.uniforms.tCube.value;
-
-        }
-
-    });
-
-    // Skybox
-
-    cubeMesh = new THREE.Mesh(new THREE.BoxBufferGeometry(100, 100, 100), cubeMaterial);
-    sceneCube.add(cubeMesh);
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.autoClear = false;
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    renderer.gammaOutput = true;
-
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 500;
-    camera.position.y = 0;
-    scene.add(camera);
-
-    cameraCube = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 100000);
-
-    //controls = new THREE.OrbitControls(camera);
-    //controls.minDistance = 500;
-    //controls.maxDistance = 2500;
-
-
-    var directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.x = 0;
-    directionalLight.position.y = 0;
-    directionalLight.position.z = 1;
-    directionalLight.position.normalize();
-    scene.add(directionalLight);
-
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200 || xhr.status == 0) {
-                var rep = xhr.response; // || xhr.mozResponseArrayBuffer;
-                console.log(rep);
-                parseStlBinary(rep);
-                //parseStl(xhr.responseText);
-                mesh.rotation.x = 5;
-                mesh.rotation.z = .25;
-                mesh.position.z = -1500;
-                mesh.position.y = -400;
-                console.log('done parsing');
-            }
-        }
-    }
-    xhr.onerror = function (e) {
-        console.log(e);
-    }
-
-    xhr.open("GET", 'bellator.stl', true);
-    xhr.responseType = "arraybuffer";
-    //xhr.setRequestHeader("Accept","text/plain");
-    //xhr.setRequestHeader("Content-Type","text/plain");
-    //xhr.setRequestHeader('charset', 'x-user-defined');
-    xhr.send(null);
-
-    /*
-    AFRAME.registerComponent('example-geometry', {
-        update: function () {
-            var mesh = this.el.getOrCreateObject3D('mesh', THREE.Mesh);
-            mesh.geometry = new THREE.Geometry();
-        }
-    });
-    */
-
-    //renderer = new THREE.WebGLRenderer(); //new THREE.CanvasRenderer();
-    //renderer.setSize(window.innerWidth, window.innerHeight);
-
-    //document.body.appendChild(renderer.domElement);
-
-    stats = new Stats();
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.top = '0px';
-    document.body.appendChild(stats.domElement);
-
-    document.body.appendChild(WEBVR.createButton(renderer));
-    renderer.vr.enabled = true;
-
-    /*
-    // vive controllers
-    controller1 = new THREE.ViveController(0);
-    controller1.standingMatrix = renderer.vr.getStandingMatrix();
-    controller1.userData.id = 0;
-    scene.add(controller1);
-
-    controller2 = new THREE.ViveController(1);
-    controller2.standingMatrix = renderer.vr.getStandingMatrix();
-    controller2.userData.id = 1;
-    scene.add(controller2);
-    
-
-    var loader = new THREE.OBJLoader();
-    loader.setPath('vive-controller/');
-    loader.load('vr_controller_vive_1_5.obj', function (object) {
-
-        var loader = new THREE.TextureLoader();
-        loader.setPath('vive-controller/');
-
-        var controller = object.children[0];
-        controller.material.map = loader.load('onepointfive_texture.png');
-        controller.material.specularMap = loader.load('onepointfive_spec.png');
-        controller.castShadow = true;
-        controller.receiveShadow = true;
-
-        controller1.add(controller.clone());
-        controller2.add(controller.clone());
-    });
-    */
-
-    //renderer.setAnimationLoop(render);
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-
-    render();
-}
-
-function render() {
-
-    //handleController(controller1);
-    //handleController(controller2);
-
-    //mesh.rotation.x += 0.01;
-        if (mesh) {
-        mesh.rotation.z += 0.02;
-    }
-    //light1.position.z -= 1;
-
-    //controls.update();
-    //camera.lookAt(scene.position);
-    cameraCube.rotation.copy(camera.rotation);
-
-    renderer.render(sceneCube, cameraCube);
-
-    renderer.render(scene, camera);
-    stats.update();
-}
-
-function handleController(controller) {
-    controller.update();
-
-}
